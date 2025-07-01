f0376033473aba01d86c0f1f9244934584ec5211
Nathan <paranhoscontato.n@gmail.com>
2025-06-30 23:11:04 -0300

    ter commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -133,7 +133,7 @@ const Footer: React.FC = () => {
                   <div className="w-10 h-10 bg-blue-700 rounded-lg flex items-center justify-center flex-shrink-0 mt-1">
                     <info.icon className="w-5 h-5 text-blue-200" />
                   </div>
-                  <div className="flex-1">
+                  <div className="flex-1 min-w-0">
                     <button 
                       onClick={() => handleNavClick(info.href)}
                       className="text-blue-100 hover:text-white transition-colors duration-300 break-words text-left text-sm"
